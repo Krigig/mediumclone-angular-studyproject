dbd2ce89277ebe0a96b47699d835e7a8d6618ccf
Kristina <vasileva_kri@list.ru>
2023-06-22 13:17:42 +0400

    20 lesson: create persistence service

--- src/app/shared/types/currentUser.interface.ts
+++ src/app/shared/types/currentUser.interface.ts
@@ -1,12 +1,10 @@
 export interface CurrentUSerInterface {
-  user: {
-    id: number;
-    email: string;
-    createdAt: string;
-    username: string;
-    updatedAt: string;
-    bio: string | null;
-    image: string | null;
-    token: string;
-  };
+  id: number;
+  email: string;
+  createdAt: string;
+  username: string;
+  updatedAt: string;
+  bio: string | null;
+  image: string | null;
+  token: string;
 }
